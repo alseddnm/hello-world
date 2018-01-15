66e1de889abed8139b134e6f3862545af6dfed02
Nizar <nmalsedd@us.ibm.com>
2018-01-15 00:23:02 -0600

    initial commit

--- src/main/java/com/fun/coding/deadlock/DeadLocks.java
+++ src/main/java/com/fun/coding/deadlock/DeadLocks.java
@@ -4,87 +4,87 @@ import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadInfo;
 import java.lang.management.ThreadMXBean;
 import java.util.Arrays;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by nizar on 1/11/18.
  */
+@Scope(value = "singleton")
+@Component
 public class DeadLocks {
+
   private static Logger LOGGER = LoggerFactory.getLogger(DeadLocks.class);
+  private final int monitorEvery = 5; // run the monitor service every 5 seconds. Should be configurable.
+  private ScheduledExecutorService monitorService = Executors.newScheduledThreadPool(1);
+  private Object lock1 = new Object();
+  private Object lock2 = new Object();
 
-  private static Semaphore threadLock = new Semaphore(1);
 
-  public static void startThreads() {
-    final Object lock1 = new Object();
-    final Object lock2 = new Object();
-
-    Thread thread1 = new Thread(new Runnable() {
-      @Override
-      public void run() {
-        synchronized (lock1) {
-          LOGGER.info("Thread1 acquired lock1");
-          try {
-            TimeUnit.MILLISECONDS.sleep(50);
-          } catch (InterruptedException ignore) {
-          }
-          synchronized (lock2) {
-            LOGGER.info("Thread1 acquired lock2");
-          }
-        }
-      }
-
-    });
-    thread1.start();
-
-    Thread thread2 = new Thread(new Runnable() {
-      @Override
-      public void run() {
-        synchronized (lock2) {
-          LOGGER.info("Thread2 acquired lock2");
-          try {
-            TimeUnit.MILLISECONDS.sleep(50);
-          } catch (InterruptedException ignore) {
-          }
-          synchronized (lock1) {
-            LOGGER.info("Thread2 acquired lock1");
-          }
-        }
-      }
-    });
-    thread2.start();
-
-    // Wait a little for threads to deadlock.
-//    try {
-//      TimeUnit.MILLISECONDS.sleep(100);
-//    } catch (InterruptedException ignore) {}
-//    detectDeadlock();
-  }
-
+  private Semaphore threadLock = new Semaphore(1);
 
   /**
-   * @return
+   *
    */
-  public static String monitorDeadlocks() {
-    while (true) {
-      try {
-        threadLock.acquire();
-        ThreadMXBean bean = ManagementFactory.getThreadMXBean();
-        long[] threadIds = bean.findDeadlockedThreads(); // Returns null if no threads are deadlocked
-        if (threadIds != null) {
-          ThreadInfo[] info = bean.getThreadInfo(threadIds);
-          logDeadlockAndQuit(bean, threadIds, info);
-          String message = String.format("Threads in deadlocks: {%s}", Arrays.toString(threadIds));
-          return message;
+  private Thread thread1 = new Thread(new Runnable() {
+    @Override
+    public void run() {
+      synchronized (lock1) {
+        LOGGER.info("Thread1 acquired lock1");
+        try {
+          TimeUnit.MILLISECONDS.sleep(50);
+        } catch (InterruptedException ignore) {
         }
-      } catch (InterruptedException e) {
-        LOGGER.error("Interrupted while monitoring threads dead lock...!");
-      } finally {
-        threadLock.release();
+        synchronized (lock2) {
+          LOGGER.info("Thread1 acquired lock2");
+        }
       }
     }
+
+  });
+
+  /**
+   *
+   */
+  private Thread thread2 = new Thread(new Runnable() {
+    @Override
+    public void run() {
+      synchronized (lock2) {
+        LOGGER.info("Thread2 acquired lock2");
+        try {
+          TimeUnit.MILLISECONDS.sleep(50);
+        } catch (InterruptedException ignore) {
+        }
+        synchronized (lock1) {
+          LOGGER.info("Thread2 acquired lock1");
+        }
+      }
+    }
+  });
+
+  /**
+   *
+   */
+  public void startAndMonitorThreads() {
+    try {
+      threadLock.acquire();
+      thread1.start();
+      thread2.start();
+
+      // monitor job will start after 2 seconds and run every 5 seconds.
+      monitorService.scheduleAtFixedRate(new MonitorThread(), 2, monitorEvery, TimeUnit.SECONDS);
+    } catch (InterruptedException e) {
+      LOGGER.error("Interrupted while starting the Monitor thread.");
+    } finally {
+      threadLock.release();
+    }
+
   }
 
   /**
@@ -92,7 +92,7 @@ public class DeadLocks {
    * @param threadIds
    * @param info
    */
-  private static void logDeadlockAndQuit(ThreadMXBean bean, long[] threadIds, ThreadInfo[] info) {
+  private void logDeadlockAndQuit(ThreadMXBean bean, long[] threadIds, ThreadInfo[] info) {
     LOGGER.error("Threads in deadlocks: {}", Arrays.toString(threadIds));
 
     for (ThreadInfo threadInfo : info) {
@@ -102,22 +102,61 @@ public class DeadLocks {
       // Attempt to log the stack trace, when available
       for (StackTraceElement stackTraceElement : threadInfo.getStackTrace()) {
         LOGGER.error("{}::{} @ {}:{}",
+
           stackTraceElement.getClassName(), stackTraceElement.getMethodName(),
           stackTraceElement.getFileName(), stackTraceElement.getLineNumber());
       }
     }
 
-//    System.exit(0);
   }
 
   /**
-   * @param ms
+   *
    */
-  private static void waitUninterruptedlyForMs(int ms) {
+  private void stop() {
     try {
-      Thread.sleep(ms);
+      LOGGER.info("Stopping threads.");
+      if (thread1 != null) {
+        LOGGER.info("Stopping thread1.");
+        thread1.interrupt();
+        thread1.join();
+      }
+      if (thread2 != null) {
+        LOGGER.info("Stopping thread2.");
+        thread2.interrupt();
+        thread2.join();
+      }
+      monitorService.shutdownNow();
+      monitorService.awaitTermination(5, TimeUnit.SECONDS);
     } catch (InterruptedException e) {
-      // Ignore it
+      LOGGER.info("Interrupted while stopping");
     }
   }
+
+  /**
+   *
+   */
+  private class MonitorThread implements Runnable {
+    @Override
+    public void run() {
+      try {
+        threadLock.acquire();
+        ThreadMXBean bean = ManagementFactory.getThreadMXBean();
+        long[] threadIds = bean.findDeadlockedThreads(); // Returns null if no threads are deadlocked
+        if (threadIds != null) {
+          ThreadInfo[] info = bean.getThreadInfo(threadIds);
+          logDeadlockAndQuit(bean, threadIds, info);
+          stop();
+        } else {
+          LOGGER.info("NO deadlocks");
+        }
+        threadLock.release();
+      } catch (InterruptedException ie) {
+        LOGGER.error("Monitor Thread got interrupted.", ie);
+      } finally {
+        threadLock.release();
+      }
+    }
+  }
+
 }
